1d2af1735e4e4287f4674bb1d7da4e7f6152960c
mikaelrev <mikael.myrvang@gmail.com>
2022-12-13 13:43:48 +0100

    Added screenshots of two of the projects

--- src/Projects.js
+++ src/Projects.js
@@ -10,21 +10,21 @@ function Projects() {
       <div className="projects__grid">
         <ProjectCard
           Description="An ecommerce website where you can make an account and buy climbing gear"
-          ImageUrl="https://cdn.forestresearch.gov.uk/2021/11/forestry-home-1.jpg"
+          ImageUrl="images/semester-project2.png"
           Title="Semester Project 2"
           SiteUrl="https://admirable-swan-22efb8.netlify.app"
           RepoUrl="https://github.com/mikaelrev/semester-project2"
         />
         <ProjectCard
           Description="Change this"
-          ImageUrl="https://cdn.forestresearch.gov.uk/2021/11/forestry-home-1.jpg"
+          ImageUrl=""
           Title="JS Frameworks CA"
           SiteUrl=""
           RepoUrl="https://github.com/Noroff-FEU-Assignments/js-frameworks-course-assignment-mikaelrev"
         />
         <ProjectCard
           Description="A social media website where people with similar music tastes can get together and share what they're listening to"
-          ImageUrl="./images/Screenshot 2022-12-13 at 13-00-18 React App.png"
+          ImageUrl="images/project-exam2.png"
           Title="Project Exam 2"
           SiteUrl="https://magnificent-sundae-78f153.netlify.app/"
           RepoUrl="https://github.com/Noroff-FEU-Assignments/project-exam-2-mikaelrev"
